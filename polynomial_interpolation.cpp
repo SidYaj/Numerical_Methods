b29abec90570792f6eed2e292af41c2241174a45
Siddharth Yajaman <siddharthyajaman@Siddharths-MacBook-Air.local>
2023-02-15 12:46:31 +0800

    added ability to obtain interpolating polynomial

--- polynomial_interpolation.cpp
+++ polynomial_interpolation.cpp
@@ -18,3 +18,92 @@ double lagrange_interpolation(std::vector<double> x_vec, std::vector<double> y_v
     }
     return L;
 }
+
+Eigen::VectorXd lagrange_coefficients(Eigen::VectorXd x_vec, Eigen::VectorXd y_vec)
+{
+    int k = x_vec.size();
+    Eigen::MatrixXd X(k, k);
+    for(int i = 0; i < k; i++)
+    {
+        for(int j = 0; j < k; j++)
+        {
+            X(i, j) = pow(x_vec(i), j);
+        }
+    }
+    Eigen::VectorXd a = X.colPivHouseholderQr().solve(y_vec);
+    return a;
+}
+
+void output_polynomial(Eigen::VectorXd a)
+{
+    int k = a.size();
+    std::cout << "p(x) = ";
+    bool flag = false; // to check for first non-zero coefficient
+    for(int i = 0; i < k; i++)
+    {
+        if(abs(a(i)) < 1e-10)
+        {
+            continue;
+        }
+        if(i == 0)
+        {
+            std::cout << a(i);
+        }
+        else
+        {
+            if(flag)
+            {
+                if(a(i) > 0)
+                {
+                    std::cout << " + ";
+                }
+                else
+                {
+                    std::cout << " - ";
+                }
+                if(abs(abs(a(i)) - 1) > 1e-10)
+                {
+                    std::cout << abs(a(i));
+                }
+                if(i == 1)
+                {
+                    std::cout << "x";
+                }
+                else
+                {
+                    std::cout << "x^" << i;
+                }
+            }
+            else
+            {
+                if(abs(abs(a(i)) - 1) > 1e-10)
+                {
+                    if(a(i) > 0)
+                    {
+                        std::cout << a(i);
+                    }
+                    else
+                    {
+                        std::cout << "-" << abs(a(i));
+                    }
+                }
+                else
+                {
+                    if(a(i) == -1)
+                    {
+                        std::cout << "-";
+                    }
+                }
+                if(i == 1)
+                {
+                    std::cout << "x";
+                }
+                else
+                {
+                    std::cout << "x^" << i;
+                }
+            }
+        }
+        flag = true;
+    }
+}
--- polynomial_interpolation.hpp
+++ polynomial_interpolation.hpp
@@ -1,9 +1,14 @@
 #ifndef POLYNOMIAL_INTERPOLATION_HPP
 #define POLYNOMIAL_INTERPOLATION_HPP
 
-#include<vector>
+#include <vector>
+#include <Eigen/Dense>
 #include "polynomial_interpolation.hpp"
 
 double lagrange_interpolation(std::vector<double> x_vec, std::vector<double> y_vec, double x);
+
+Eigen::VectorXd lagrange_coefficients(Eigen::VectorXd x_vec, Eigen::VectorXd y_vec);
+
+void output_polynomial(Eigen::VectorXd a);
 
 #endif
